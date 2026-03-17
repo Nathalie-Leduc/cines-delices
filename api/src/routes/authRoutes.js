d99cd5e586527d3ce8549a75db4594cbeac22726
student <nathalie.leduc@oclock.school>
2026-03-17 16:31:09 +0100

    feat/auth/auth.validator

--- api/src/routes/authRoutes.js
+++ api/src/routes/authRoutes.js
@@ -1,26 +1,19 @@
-// Ordres des middlewares sur chaque route :
-//   1. validate(schema)   → Zod vérifie et nettoie les données
-//   2. authMiddleware     → JWT vérifie l'identité (si besoin)
-//   3. controller         → logique métier (données déjà propres)
-
 import { Router } from "express";
 import { register, login, logout, getMe, updateMe, deleteMe } from "../controllers/authController.js";
-import { authMiddleware } from "../middlewares/authMiddleware.js";
-import { validate } from "../middlewares/validateMiddleware.js";
-import { registerSchema, loginSchema,updateMeSchema } from "../validators/authValidator.js";
+import { registerSchema, loginSchema,updateMeSchema } from "../validators/auth.validator.js";
 
-const router = Router();
+const router = Router;
 
-router.post('/register', validate(registerSchema), register);
+router.post('/register', register);
 
-router.post('/login', validate(loginSchema), login);
+router.post('/login', login);
 
 router.get('/logout', logout);
 
-router.get('/me', authMiddleware, getMe);
+router.get('/me', getMe);
 
-router.patch('/me', authMiddleware, validate(updateMeSchema), updateMe);
+router.put('/me', updateMe);
 
-router.delete('/me', authMiddleware, deleteMe);
+router.delete('/me', deleteMe);
 
 export default router;
