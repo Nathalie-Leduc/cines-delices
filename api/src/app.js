fdf736e297ddc2f39b9e10e359a34cf8b4b8b345
Orianne Jaunet <orfer@orange.fr>
2026-03-18 10:56:58 +0100

    ajout 'profil' sur navbar, adaptation du formulaire 'mes infirmatuons'' (#90)

--- api/src/app.js
+++ api/src/app.js
@@ -3,7 +3,6 @@ import cors from 'cors';
 import routes from "./routes/index.js"
 
 const app = express();
-const PORT = 3000;
 
 app.use(cors({
   origin: 'http://localhost:5173'
@@ -15,10 +14,6 @@ app.use("/api", routes);
 
 app.get('/api/health', (_req, res) => {
   res.json({ status: 'ok' });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
 });
 
 export default app;
