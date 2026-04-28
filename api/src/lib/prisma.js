800bd987af19572a9d70f2ea2acc45067c6db587
student <nathalie.leduc@oclock.school>
2026-04-28 17:49:23 +0200

    fix: import PrismaClient compatible Prisma 7

--- api/src/lib/prisma.js
+++ api/src/lib/prisma.js
@@ -1,14 +1,14 @@
 import pg from 'pg';
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import pkg from "@prisma/client";
+const { PrismaClient } = pkg;
 
-//pg.Pool gère automatiquement les connexions ouvertes/fermées
-const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL})
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 const adapter = new PrismaPg(pool);
 
 export const prisma = new PrismaClient({
   adapter,
   log: process.env.NODE_ENV === 'development'
-    ? ['query', 'warn', 'error']  // logs SQL visibles en dev
+    ? ['query', 'warn', 'error']
     : ['warn', 'error'],
 });
